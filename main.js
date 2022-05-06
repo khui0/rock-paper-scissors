ada0a45d43bafbb56b3a2bbc0477829495097a1c
Kenny <101839505+khui0@users.noreply.github.com>
2022-05-05 20:23:16 -0400

    things

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
-var gameMode = document.querySelector(`input[name="mode"]:checked`).value;
+const player1Name = "Player 1";
+const player2Name = "Player 2";
 
+const p1Icon = document.querySelector("#p1>p");
+const p2Icon = document.querySelector("#p2>p");
+const p1Submit = document.getElementById("p1-submit");
+const p2Submit = document.getElementById("p2-submit");
+const actionButton = document.getElementById("action");
+
+const icons = ["💎", "🧻", "🔪"];
+
+var gameMode = document.querySelector(`input[name="mode"]:checked`).value;
 var p1Selection = document.querySelector(`input[name="p1-selection"]:checked`).value;
 var p2Selection = document.querySelector(`input[name="p2-selection"]:checked`).value;
 
-var p1Submit = document.getElementById("p1-submit");
-var p2Submit = document.getElementById("p2-submit");
+resetGame();
+
+document.querySelectorAll(`input[name="mode"]`).forEach(input => {
+    input.addEventListener("input", () => {
+        gameMode = document.querySelector(`input[name="mode"]:checked`).value;
+        resetGame();
+    })
+})
 
 document.querySelectorAll(`input[type="radio"]`).forEach(input => {
     input.addEventListener("input", () => {
-        gameMode = document.querySelector(`input[name="mode"]:checked`).value;
         p1Selection = document.querySelector(`input[name="p1-selection"]:checked`).value;
         p2Selection = document.querySelector(`input[name="p2-selection"]:checked`).value;
         if (gameMode == 0) {
-            document.querySelector("#p2>form").style.display = "none";
-            p1Submit.style.display = "none";
-            p2Submit.style.display = "none";
-        }
-        else if (gameMode == 1) {
-            document.querySelector("#p2>form").style.display = "block";
-            p1Submit.style.display = "inline-block";
-            p2Submit.style.display = "inline-block";
+            p1Icon.textContent = icons[p1Selection];
         }
     })
-});
+})
 
 p1Submit.addEventListener("click", () => {
     document.querySelector("#p1>form").style.display = "none";
@@ -33,3 +41,24 @@ p2Submit.addEventListener("click", () => {
     document.querySelector("#p2>form").style.display = "none";
     p2Submit.style.display = "none";
 })
+
+function resetGame() {
+    document.querySelector("#p1>h2").textContent = player1Name;
+    document.querySelector("#p2>h2").textContent = player2Name;
+    p1Icon.textContent = "💭"
+    p2Icon.textContent = "💭"
+    // Singleplayer
+    if (gameMode == 0) {
+        document.querySelector("#p1>form").style.display = "block";
+        document.querySelector("#p2>form").style.display = "none";
+        p1Submit.style.display = "none";
+        p2Submit.style.display = "none";
+    }
+    // Pass and play
+    else if (gameMode == 1) {
+        document.querySelector("#p1>form").style.display = "block";
+        document.querySelector("#p2>form").style.display = "block";
+        p1Submit.style.display = "inline-block";
+        p2Submit.style.display = "inline-block";
+    }
+}
